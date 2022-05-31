1f179b128e27e4fc4fdc679a8e9507d9c7d9c9fc
tlamidi <tlamidi@yahoo.co.uk>
2022-05-31 16:56:35 +0000

    remove error in line two of the question.py file

--- run.py
+++ run.py
@@ -1,19 +1,31 @@
-import gspread
-from google oauth2.service_account import credentials
+from Question import Question
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+question_prompts = [ 
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
 
-CREDS = Credentials.from_servi_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD CLIENT.open('love_sandwiches')
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b"),
+]
 
-sales = SHEET.worksheet('sales')
+def run_test(questions):
+    score = 0                                                                                                                                                                                                                  
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+print("You got" + str(score) + "/" + str(len(questions)) + "correct")
 
-data = sales.get_all_values()
+run_test(questions)
 
-print(data)
+
+
+
+
+
+
+
