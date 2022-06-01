746b8ac48ecd12d44cfab5a11b0404552a23826a
tlamidi <tlamidi@yahoo.co.uk>
2022-06-01 09:40:31 +0000

    add comment to codes.

--- question.py
+++ question.py
@@ -1,4 +1,4 @@
 class Question:
-    def __init__(self, prompt, answer):
+    def __init__(self, prompt, answer): # To store question prompt, and answer
         self.prompt = prompt 
         self.answer = answer 
--- run.py
+++ run.py
@@ -1,18 +1,18 @@
-from question import Question
+from question import Question  # To import a question class file.
 
 question_prompts = [
     "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
     "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
     "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
 ]
-
+# Question object created for each question in the array or list.
 questions = [
     Question(question_prompts[0], "a"),
     Question(question_prompts[1], "c"),
     Question(question_prompts[2], "b"),
 ]
 
-
+# function with loop to track correct answers and print result.
 def run_test(questions):
     score = 0                                                                                                                                                  
     for question in questions:
@@ -21,4 +21,4 @@ def run_test(questions):
             score += 1
     print("You got" + str(score) + "/" + str(len(questions)) + "correct")
 
-run_test(questions)
+run_test(questions) # To call the run_test function. 
